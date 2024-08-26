18e6234eaebb3ba6e33a317c1cbb0c201ff971a8
cynthiafunes <funescy@gmail.com>
2024-08-26 10:43:45 -0400

    agregar descripcion

--- 33_mc1/server.py
+++ 33_mc1/server.py
@@ -1,3 +1,6 @@
+#Configuración Básica de Sockets: Implementa un servidor de sockets básico que escuche en un puerto específico 
+#y acepte conexiones de un solo cliente. El servidor debería enviar un mensaje de bienvenida al cliente y luego cerrar la conexión.
+
 import socket
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
