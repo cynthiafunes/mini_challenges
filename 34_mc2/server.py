b6b3c543bf527a4037d54d74b63dcee6cc978646
cynthiafunes <funescy@gmail.com>
2024-08-26 11:15:05 -0400

    Agregar descripcion

--- 34_mc2/server.py
+++ 34_mc2/server.py
@@ -1,3 +1,6 @@
+#Cliente Simple de Chat: Desarrolla un cliente que se conecte a un servidor de sockets y permita al usuario
+#enviar un mensaje simple a través de la terminal. Una vez enviado, el cliente debería cerrar la conexión.
+
 import socket
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
